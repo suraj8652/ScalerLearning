1ed330ee5dae75caa6a0e5e80ff53724e6298b19
suraj_pathak <pathaksuraj8652@gmail.com>
2024-07-21 14:37:43 +0530

    Print number 1 to 100 using ExecutorService Interface

---
+++ src/Concurrency2/Client.java
@@ -0,0 +1,23 @@
+package Concurrency2;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Client {
+    public static void main(String[] args) {
+        //practically using 100 tread is not good practise as this jobs are very simple
+        //We can use ServiceExecutor
+        NumberPrinter num;
+        //Thread thread;
+
+        ExecutorService service = Executors.newFixedThreadPool(10);
+
+        for(int i = 1; i <= 100; i++){
+            num = new NumberPrinter(i);
+            //thread = new Thread(num);
+            service.submit(num);
+            //thread.start();
+
+        }
+    }
+}
